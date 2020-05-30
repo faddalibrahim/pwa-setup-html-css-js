dbfa7e51825ec8c4b79bdddf2f6a4f54d0a33c11
Faddal Ibrahim <41176161+faddalibrahim@users.noreply.github.com>
2020-05-30 02:47:17 +0000

    pwa-html-css-js full setup ready

---
+++ read_me.md
@@ -0,0 +1,13 @@
+I used the realfavicongenerator.com to generate the best optimized icons for the pwa
+i have to use the below codes in my header
+and also transfer the icons from the favicon zip package to the img/iconsss folder
+the package also has a web manifest besides the one i manually created
+<link rel="apple-touch-icon" sizes="180x180" href="img/iconsss/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="img/iconsss/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="img/iconsss/favicon-16x16.png">
+<link rel="manifest" href="img/iconsss/site.webmanifest">
+<link rel="mask-icon" href="img/iconsss/safari-pinned-tab.svg" color="#5bbad5">
+<link rel="shortcut icon" href="img/iconsss/favicon.ico">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="msapplication-config" content="img/iconsss/browserconfig.xml">
+<meta name="theme-color" content="#ffffff">
---
+++ sw.js
@@ -0,0 +1,46 @@
+const appShell = 'app-shell-data';
+const assets = [
+	'/',
+	'/css/index.css',
+	'/js/index.js',
+	'/js/reg_sw.js',
+	'/fonts/Roboto-Regular.ttf',
+	'/fontawesome/css/all.min.css',
+	'/fontawesome/webfonts/fa-solid-900.ttf',
+	'/fontawesome/webfonts/fa-solid-900.woff',
+	'/fontawesome/webfonts/fa-solid-900.woff2',
+	'/fontawesome/webfonts/fa-brands-400.ttf',
+	'/fontawesome/webfonts/fa-brands-400.woff',
+	'/fontawesome/webfonts/fa-brands-400.woff2'
+];
+
+//listening to install event
+self.addEventListener('install', e => {
+	e.waitUntil(
+		caches.open(appShell).then(cache => {
+			cache.addAll(assets)
+		})
+	)
+})
+
+
+// listening to activate event
+self.addEventListener('activate', e => {
+	e.waitUntil(
+		caches.keys().then(keys => {
+			return Promise.all(keys
+				.filter(key => key !== appShell)
+				.map(key => caches.delete(key))
+			)
+		})
+	)
+})
+
+//listening to fetch event
+self.addEventListener('fetch', e => {
+	e.respondWith(
+	    caches.match(e.request).then(response => {
+	        return response || fetch(e.request)
+	    })
+	)
+})
